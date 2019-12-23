881e7493a1d91ebe18738df26f0823d65bab12cb
FalconRay <falconray@yahoo.com>
2019-12-23 18:57:41 +0800

    merge docker deployment script from bak_sysCfg and make supoort for LinuxMint.

--- docker/ubt/docker.sh
+++ docker/ubt/docker.sh
@@ -5,14 +5,19 @@ set -o errexit
 
 #set -x
 
+. ../../libShell/echo_color.lib
+. ../../libShell/sysEnv.lib
+
+DOCKER_COMPOSE_VERSION=1.25.0
+
 install_DockerCompose_func()
 {
     # refer to https://docs.docker.com/compose/install/#install-compose
-    sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
     # https://docs.docker.com/compose/completion/#install-command-completion
-    sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+    sudo curl -L https://raw.githubusercontent.com/docker/compose/${DOCKER_COMPOSE_VERSION}/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 
     # check
     docker-compose --version
@@ -28,11 +33,12 @@ install_Docker_func()
 {
     # https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-docker-ce-1
     sudo apt-get update
-    sudo apt-get install docker-ce
+#    sudo apt-get install docker-ce
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
     sudo docker run hello-world
 
     # use Docker as a non-root user
-    echo "User:$USER"
+    #echo "User:$USER"
     sudo usermod -aG docker $USER
 
     #sudo reboot
@@ -43,7 +49,7 @@ check_Docker_Env_func()
     docker info
     docker version
 
-    sudo docker run hello-world
+    docker run --rm hello-world
 }
 
 uninstall_old_versions_func()
@@ -55,13 +61,19 @@ uninstall_old_versions_func()
 
 install_repo_func()
 {
-    sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     echo "Verify that you now have the key with the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88"
     sudo apt-key fingerprint 0EBFCD88
 
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+    if [ $(os_distributor) == "LinuxMint" ] && [ $(os_distribution_number) == "19.3" ]
+    then
+        echoG "Linux Mint..."
+        echo -e "\ndeb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee -a /etc/apt/sources.list
+    else
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    fi
+
 }
 
 install_utils_func()
@@ -71,7 +83,10 @@ install_utils_func()
 
 usage_func()
 {
-    echo "Supported functionalities:"
+    
+    echoY "./docker.sh <cmd> "
+    echo ""
+    echoY "Supported functionalities:"
     echo "[uninstallOldVersions]"
     echo "[installRepo]"
     echo "[installDocker]"
@@ -81,31 +96,32 @@ usage_func()
     echo "[installUtils]"
 }
 
-[ $# -lt 1 ] && usage_func && exit
+[ $# -lt 1 ] && echoR "Invalid args count:$# " && usage_func && exit 1
 
 case $1 in
-    uninstallOldVersions) echo "Unstalling old versions..."
+    uninstallOldVersions) echoY "Unstalling old versions..."
         uninstall_old_versions_func
         ;;
-    installRepo) echo "Installing Repo for docker installation..."
+    installRepo) echoY "Installing Repo for docker installation..."
         install_repo_func
         ;;
-    installDocker) echo "Installing Docker-ce ..."
+    installDocker) echoY "Installing Docker-ce ..."
         install_Docker_func
         ;;
-    checkDocker) echo "Checking docker env..."
+    checkDocker) echoY "Checking docker env..."
         check_Docker_Env_func
         ;;
-    installDockerCompose) echo "Installing Docker Compose ..."
+    installDockerCompose) echoY "Installing Docker Compose ..."
         install_DockerCompose_func
         ;;
-    uninstallDockerCompose) echo "Uninstalling Docker Compose ..."
+    uninstallDockerCompose) echoY "Uninstalling Docker Compose ..."
         uninstall_DockerCompose_func
         ;;
-    installUtils) echo "Installing useful utils ..."
+    installUtils) echoY "Installing useful utils ..."
         install_utils_func
         ;;
-    *) echo "Unknown cmd: $1"
+    *) echoR "Unknown cmd: $1"
+        usage_func
 esac
 
 
