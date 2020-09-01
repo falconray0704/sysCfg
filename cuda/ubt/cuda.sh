e856f2f24e5d168eec2cd28e98b571db250b639e
FalconRay <falconray@yahoo.com>
2020-09-01 16:53:09 +0800

    upgrade cuda to 11.0.3

--- cuda/ubt/cuda.sh
+++ cuda/ubt/cuda.sh
@@ -7,10 +7,14 @@ set -o errexit
 #set -x
 
 . ../../../libShell/echo_color.lib
-. ../../../libShell/sysEnv.lib
+#. ../../../libShell/sysEnv.lib
+. ../../../libShell/network.lib
 
-CUDA_MAIN_VERSION="10.2"
-REPO_VERSION="${CUDA_MAIN_VERSION}.89-1"
+#CUDA_MAIN_VERSION="10.2"
+#REPO_VERSION="${CUDA_MAIN_VERSION}.89-1"
+
+CUDA_MAIN_VERSION="11.0.3"
+REPO_VERSION="${CUDA_MAIN_VERSION}-450.51.06-1"
 
 install_cuda_func()
 {
@@ -24,6 +28,17 @@ install_cuda_func()
 #    echo 'export PATH=/usr/local/cuda-10.2/bin${PATH:+:${PATH}}' >> $HOME/.bashrc
     echo "export PATH=/usr/local/cuda-${CUDA_MAIN_VERSION}/bin\${PATH:+:\${PATH}}" >> $HOME/.bashrc
 
+}
+
+install_cuda_new_func()
+{
+    ${DOWNLOAD_RESOURCE} https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+    sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    ${DOWNLOAD_RESOURCE} https://developer.download.nvidia.com/compute/cuda/${CUDA_MAIN_VERSION}/local_installers/cuda-repo-ubuntu1804-11-0-local_${REPO_VERSION}_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1804-11-0-local_${REPO_VERSION}_amd64.deb
+    sudo apt-key add /var/cuda-repo-ubuntu1804-11-0-local/7fa2af80.pub
+    sudo apt-get update
+    sudo apt-get -y install cuda
 }
 
 usage_func()
@@ -40,7 +55,7 @@ usage_func()
 
 case $1 in
     install) echoY "Installing CUDA ${REPO_VERSION} ..."
-        install_cuda_func
+        install_cuda_new_func
         ;;
     *) echoR "Unknown cmd: $1"
         usage_func
