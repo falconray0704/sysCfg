66a6223a9cb059a2b06f1a690f3baba75b6e9f71
FalconRay <falconray@yahoo.com>
2021-05-19 10:30:55 +0800

    Fixed a bug of installing ss-tunnel and ss-rules. ss-rules must be installed at the end.

--- openwrt/pi/target_04_ss.sh
+++ openwrt/pi/target_04_ss.sh
@@ -3,8 +3,9 @@
 
 # Shadowsocks
 opkg update
-opkg install shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-rules shadowsocks-libev-ss-tunnel
+opkg install shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-tunnel
 opkg install luci-app-shadowsocks-libev
+opkg install shadowsocks-libev-ss-rules
 
 cp ./shadowsocks-libev /etc/config
 /etc/init.d/shadowsocks-libev restart
