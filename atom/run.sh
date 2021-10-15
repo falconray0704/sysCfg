d3bc8396653593e10355c92bc2f1e01ba71e8c80
FalconRay <falconray@yahoo.com>
2021-10-15 10:54:16 +0800

    Added atom install maintaining.

---
+++ atom/run.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+# trace each command execute, same as `bash -v myscripts.sh`
+#set -o verbose
+# trace each command execute with attachment infomations, same as `bash -x myscripts.sh`
+#set -o xtrace
+
+#set -o
+set -e
+#set -x
+
+export LIBSHELL_ROOT_PATH=$(realpath "${PWD}/../libShell")
+echo "LIBSHELL_ROOT_PATH: ${LIBSHELL_ROOT_PATH}"
+. ${LIBSHELL_ROOT_PATH}/echo_color.lib
+. ${LIBSHELL_ROOT_PATH}/utils.lib
+. ${LIBSHELL_ROOT_PATH}/sysEnv.lib
+
+# Checking environment setup symbolic link and its file exists
+if [ -L ".env_setup" ] && [ -f ".env_setup" ]
+then
+#    echoG "Symbolic .env_setup exists."
+    . ./.env_setup
+else
+    echoR "Setup environment informations by making .env_setup symbolic link to specific .env_setup_xxx file(eg: .env_setup_amd64_ubt_1804) ."
+    exit 1
+fi
+
+
+
+SUPPORTED_CMD="install"
+SUPPORTED_TARGETS="repo,atom"
+
+EXEC_CMD=""
+EXEC_ITEMS_LIST=""
+
+install_repo()
+{
+    wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+    sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+    sudo apt-get update
+}
+
+install_atom()
+{
+    sudo apt-get install -y atom
+}
+
+usage_func()
+{
+
+    echoY "Usage:"
+    echoY './run.sh -c <cmd> -l "<item list>"'
+    echoY "eg:\n./run.sh -c install -l \"repo\""
+    echoY "eg:\n./run.sh -c install -l \"atom\""
+    echoY "eg:\n./run.sh -c install -l \"repo,atom\""
+
+    echoC "Supported cmd:"
+    echo "${SUPPORTED_CMD}"
+    echoC "Supported items:"
+    echo "${SUPPORTED_TARGETS}"
+    
+}
+
+no_args="true"
+while getopts "c:l:" opts
+do
+    case $opts in
+        c)
+              # cmd
+              EXEC_CMD=$OPTARG
+              ;;
+        l)
+              # items list
+              EXEC_ITEMS_LIST=$OPTARG
+              ;;
+        :)
+            echo "The option -$OPTARG requires an argument."
+            exit 1
+            ;;
+        ?)
+            echo "Invalid option: -$OPTARG"
+            usage_func
+            exit 2
+            ;;
+        *)    #unknown error?
+              echoR "unkonw error."
+              usage_func
+              exit 1
+              ;;
+    esac
+    no_args="false"
+done
+
+[[ "$no_args" == "true" ]] && { usage_func; exit 1; }
+#[ $# -lt 1 ] && echoR "Invalid args count:$# " && usage_func && exit 1
+
+
+case ${EXEC_CMD} in
+    "install")
+        install_items ${EXEC_CMD} ${EXEC_ITEMS_LIST}
+        ;;
+    "*")
+        echoR "Unsupport cmd:${EXEC_CMD}"
+        ;;
+esac
+
+
+
